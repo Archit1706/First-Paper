beceb6296d715888daefc7ac7cff274761a8810b
Archit <architrathod77@gmail.com>
2023-01-03 02:33:42 +0530

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
 
+import Navbar from '../components/Navbar'
+import Main from '../components/Main'
+import Papers from '../components/Papers'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -10,7 +16,17 @@ export default function Home() {
         <link rel="icon" href="/favicon2.png" />
       </Head>
 
+      <Navbar />
+
+      <Main />
+
+      <Papers />
+
+      <About />
+
+      <Contact />
       
+      <Footer />
 
     </div>
   )
