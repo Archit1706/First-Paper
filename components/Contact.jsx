34bcf00b453043c4d9e15935fa5ce35e9b11025f
Archit <architrathod77@gmail.com>
2023-01-03 09:09:44 +0530

    Components added

---
+++ components/Contact.jsx
@@ -0,0 +1,174 @@
+import React, { useState, useEffect } from "react";
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { team } from "../data";
+import { BsFillFilePersonFill } from "react-icons/bs";
+
+const Contact = () => {
+    const [formData, setFormData] = useState({});
+
+    useEffect(() => {
+        console.log(formData);
+    }, [formData]);
+
+    const changeHandler = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    return (
+        <section id="contact" className="text-gray-600 relative">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col text-center w-full mb-12">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                        Contact Us
+                    </h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        Want to suggest changes or submit a your research paper?
+                    </p>
+                    <p>Feel free to contact us.</p>
+                </div>
+
+                <form className="lg:w-1/2 md:w-2/3 mx-auto">
+                    <div className="flex flex-wrap -m-2">
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                                <label
+                                    htmlFor="name"
+                                    className="leading-7 text-sm text-gray-600"
+                                >
+                                    Name
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                />
+                            </div>
+                        </div>
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                                <label
+                                    htmlFor="email"
+                                    className="leading-7 text-sm text-gray-600"
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    onChange={changeHandler}
+                                />
+                            </div>
+                        </div>
+                        <div className="p-2 w-full">
+                            <div className="relative">
+                                <label
+                                    htmlFor="message"
+                                    className="leading-7 text-sm text-gray-600"
+                                >
+                                    Message
+                                </label>
+                                <textarea
+                                    id="message"
+                                    name="message"
+                                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                                    onChange={changeHandler}
+                                ></textarea>
+                            </div>
+                        </div>
+                        <div className="p-2 w-full">
+                            <button className="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
+                                Submit
+                            </button>
+                        </div>
+
+                        <div className="mx-auto my-4">
+                            <p className="font-bold text-center text-green-500">
+                                OR
+                            </p>
+                        </div>
+
+                        <section className="text-gray-600">
+                            <div className="container px-5 py-4 mx-auto">
+                                <div className="flex flex-col text-center w-full mb-10">
+                                    <h1 className="text-2xl font-medium title-font mb-4 text-gray-900">
+                                        Our Team
+                                    </h1>
+                                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base italic">
+                                        &quot; No one can whistle a symphony. It
+                                        takes a whole orchestra to play it{" "}
+                                        &quot; - H.E. Luccock
+                                    </p>
+                                </div>
+                                <div className="flex flex-wrap -m-4">
+                                    {team.map((member, index) => {
+                                        return (
+                                            <div
+                                                key={index}
+                                                className="p-4 w-1/2"
+                                            >
+                                                <div className="h-full flex flex-col items-center text-center">
+                                                    {member.img ? (
+                                                        <img
+                                                            alt={member.name}
+                                                            className="flex-shrink-0 rounded-lg w-8/12 h-56 object-cover object-center mb-2"
+                                                            src={member.img}
+                                                        />
+                                                    ) : (
+                                                        <BsFillFilePersonFill className="flex-shrink-0 rounded-lg w-8/12 h-56 object-cover object-center mb-2 text-green-500 hover:text-green-600" />
+                                                    )}
+
+                                                    <div className="w-full">
+                                                        <h2 className="title-font font-medium text-lg text-gray-900">
+                                                            {member.name}
+                                                        </h2>
+                                                        <h3 className="text-gray-500 mb-3">
+                                                            {member.tag}
+                                                        </h3>
+                                                        <p className="mb-4">
+                                                            {member.intro}
+                                                        </p>
+                                                        <span className="inline-flex">
+                                                            <a
+                                                                href={
+                                                                    member.github
+                                                                }
+                                                                className="text-gray-500 hover:text-green-500"
+                                                            >
+                                                                <FaGithub className="w-5 h-5" />
+                                                            </a>
+                                                            <a
+                                                                href={
+                                                                    member.linkedin
+                                                                }
+                                                                className="ml-4 text-gray-500 hover:text-green-500"
+                                                            >
+                                                                <FaLinkedinIn className="w-5 h-5" />
+                                                            </a>
+                                                            <a
+                                                                href={
+                                                                    member.twitter
+                                                                }
+                                                                className="ml-4 text-gray-500 hover:text-green-500"
+                                                            >
+                                                                <FaTwitter className="w-5 h-5" />
+                                                            </a>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
---
+++ components/Main.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { categories } from "../data";
+import Typewriter from "typewriter-effect";
+
+const Main = () => {
+    const [papers, setPapers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // Initialize state for form inputs
+    const [formData, setFormData] = useState({});
+
+    useEffect(() => {
+        // console.log(formData);
+    }, [formData]);
+
+    const changeHandler = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
+    return (
+        <div
+            id="home"
+            className="text-gray-600 rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2"
+        >
+            <div className="App">
+                <h1 className="text-3xl font-bold text-center mb-4">
+                    Search papers based on{" "}
+                    <span className="text-green-500 inline-flex">
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    "Topic",
+                                    "Authors",
+                                    "Subjects",
+                                    "Date Range",
+                                    "or All",
+                                ],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </span>
+                </h1>
+            </div>
+
+            <div className="-mx-3 md:flex mb-6">
+                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block leading-7 text-sm text-gray-600">
+                        Topic
+                    </label>
+                    <input
+                        className="appearance-none block w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        type="text"
+                        placeholder="Infrared, Covid-19, etc."
+                        name="topic"
+                        onChange={changeHandler}
+                    />
+                </div>
+                <div className="md:w-1/2 px-3">
+                    <label className="block leading-7 text-sm text-gray-600">
+                        Authors
+                    </label>
+                    <input
+                        className="appearance-none block w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        type="text"
+                        name="authors"
+                        placeholder="James Belk, S. K. Gupta, etc."
+                        onChange={changeHandler}
+                    />
+                </div>
+            </div>
+            <div className="-mx-3 md:flex mb-6">
+                <div className="md:w-full px-3">
+                    <label
+                        className="block leading-7 text-sm text-gray-600"
+                        htmlFor="grid-state"
+                    >
+                        Subjects
+                    </label>
+                    <div className="relative">
+                        <select
+                            className="block appearance-none w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={changeHandler}
+                        >
+                            <option></option>
+                            {categories.map((value, index) => {
+                                return <option key={index}>{value}</option>;
+                            })}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="-mx-3 md:flex mb-2">
+                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                        className="block leading-7 text-sm text-gray-600"
+                        htmlFor="grid-city"
+                    >
+                        Start Date
+                    </label>
+                    <input
+                        className="appearance-none block w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        type="date"
+                        name="startDate"
+                        placeholder="01-01-1990"
+                        onChange={changeHandler}
+                    />
+                </div>
+
+                <div className="md:w-1/2 px-3">
+                    <label className="block leading-7 text-sm text-gray-600">
+                        End Date
+                    </label>
+                    <input
+                        className="appearance-none block w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        type="date"
+                        name="endDate"
+                        placeholder="31-12-2022"
+                        onChange={changeHandler}
+                    />
+                </div>
+            </div>
+            <div className="flex items-center py-6">
+                <button className="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg" onClick={handleSearch}>
+                    Search
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
